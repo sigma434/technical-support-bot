da1df578b3883c3cb279a3adc6b8c8cbf3def1b0
sigma434 <boleslavsem@gmail.com>
2026-06-20 14:02:08 +0300

    /

--- bot.py
+++ bot.py
@@ -1,8 +1,31 @@
 import telebot
-from config import BOT_TOKEN
+from config import BOT_TOKEN, ADMIN_ID
 from lwdb import save_to_db
+from telebot import types
+from faq import faq_template
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def get_faq_keyboard():
+    """Обычная клавиатура с вопросами (внизу экрана)"""
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    
+    for question in faq_template.keys():
+        ks = types.KeyboardButton(question)
+        keyboard.add(ks)
+    
+    # Кнопка "Назад" (чтобы убрать клавиатуру)
+    keyboard.add(types.KeyboardButton("🔙 Закрыть FAQ"))
+    
+    return keyboard
+
+def format_answer(question, answer):
+    return f"{question}\n\n{answer}"
+
+def is_admin(user_id):
+    return user_id == ADMIN_ID
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -17,23 +40,30 @@ def handle_start(message):
 • Программисты — если не работает сайт или оплата
 • Отдел продаж — вопросы про товары, заказы, возвраты
 
-💬 Просто напишите свой вопрос, и я постараюсь помочь!
+💬 Просто напишите /faq, и я постараюсь помочь!
 
 📌 Попробуйте:
-• «Как оформить заказ?»
-• «Не проходит оплата»
-• «Где мой заказ?»
+• «Что такое [название продукта/сервиса]?»
+• «Как начать пользоваться [название продукта/сервиса]?»
+• «Что делать, если [название продукта/сервиса] не запускается?»
+
+❔ Остались еще вопросы попробуйте:
+• /save дальше напишите подробную ситуацию с который вы столкнулись
 
 """)
 
 @bot.message_handler(commands=['faq'])
-def connection_w_staff(message):
-    bot.send_message(message.chat.id, )
+def show_faq(message):
+    bot.send_message(
+        message.chat.id,
+        "📚 Часто задаваемые вопросы\n\n"
+        "Нажмите на интересующий вопрос:",
+        reply_markup=get_faq_keyboard(),
+    )
     
 @bot.message_handler(commands=['save'])
 def s_a_c(message):
     text = message.text.replace('/save', '')
-
 
     message_id = save_to_db(
         user_id=message.from_user.id,
@@ -41,10 +71,88 @@ def s_a_c(message):
         issue=text
     )
 
-#СТРУКТУРА ДЛЯ МЕНЯ БУДУ ПИСАТЬ КАПСОМ
-#КРЧ БЕРЕШЬ СОХРАНЯЕШЬ ФАЙЛ ПЕРЕКИДЫВЕШЬ ЕГО В ДБ ЖТО С КЬЮА
-# С ФАКЙУЮ БЕРЕШЬ ДОБАВЛЯЕШЬ КНОПОЧКИ С КЛЮЧАМИ ОН ИХ БЕРЕТ ПЕРЕКИДЫВАЕТ СЮДЫ А ТЫ БЕРЕШЬ ИХ И С ПОМОЗЬЮ ИМПОРТИРОВАННОГО СЛОВАРЯ ОТВЕЧАЕШЬ НА ЕГО ВОПРОС
-# вроде как все рассказал
+    bot.send_message(message.chat.id, "Мы передали вашу проблему специалистам. Они свяжутся с вами в ближайшее время.")
+
+
+    user_info = f"@{message.from_user.username}" if message.from_user.username else message.from_user.first_name
+    bot.send_message(
+        ADMIN_ID,
+        f"Новый вопрос от пользователя!\n\n"
+        f"Пользователь: {user_info}\n"
+        f"ID: {message.from_user.id}\n"
+        f"Вопрос: {text}\n\n"
+        f"Чтобы ответить, используйте:\n"
+        f"/answer {message.from_user.id} [текст] - ответить"
+    )
+
+
+@bot.message_handler(commands=['answer'])
+def answer_to_user(message):
+    if not is_admin(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет прав для этой команды.")
+        return
+    
+    parts = message.text.split(maxsplit=2)
+    if len(parts) < 3:
+        bot.send_message(
+            message.chat.id,
+            "Укажите ID пользователя и текст ответа.\n"
+            "Пример: /answer 11... Ваш заказ уже отправлен."
+        )
+        return
+    
+    try:
+        user_id = int(parts[1])
+    except ValueError:
+        bot.send_message(
+            message.chat.id,
+            "Неверный ID пользователя. Введите число."
+        )
+        return
+    
+    answer_text = parts[2]
+    
+    try:
+        bot.send_message(
+            user_id,
+            f"Ответ от специалиста:\n{answer_text}\n"
+            f"Если у вас остались вопросы, напишите их снова."
+        )
+        
+        bot.send_message(message.chat.id,f"Ответ отправлен пользователю (ID: {user_id})!")
+        
+    except Exception as error:
+        bot.send_message(
+            message.chat.id,
+            f"Ошибка при отправке ответа: {str(error)}"
+        )
+
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    text = message.text
+    
+    
+    if text == "🔙 Закрыть FAQ":
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(
+            message.chat.id,
+            "FAQ закрыт.\nНапишите /faq чтобы открыть снова.",
+            reply_markup=keyboard
+        )
+        return
+    
+    if text in faq_template:
+        answer = faq_template[text]
+        response = format_answer(text, answer)
+        bot.send_message(message.chat.id, response)
+    else:
+        bot.send_message(
+            message.chat.id,
+            "Я не нашел ответ на ваш вопрос.\n\n"
+            "Напишите /faq чтобы увидеть список всех вопросов."
+        )
 
 
 bot.infinity_polling()
